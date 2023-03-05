d48698c010ac525939fd9f533392b4c7d5a70a8e
Jack Zellweger <20098240+jackzellweger@users.noreply.github.com>
2023-03-05 14:08:11 -0500

    Updated files

--- src/automate_bin_weight.sh
+++ src/automate_bin_weight.sh
@@ -3,27 +3,21 @@ clear
 echo Will generate and analyze $1 waveforms, binning them into $2 bins using $3 cores
 
 numCores=$3
-
 numberEdges=$(( $1 * ($1 - 1)/2 ))
-
 binSize=$(( $numberEdges / $3 ))
 
 for i in `seq 20 0`
 do
 	if let '$numberEdges % i == 0'
 		$numCores=
-
 done
 
 ifIt=$(($numberEdges % $3))
-
 #mult=$(($numberEdges / $3))
 
 if let '$ifIt != 0'; then
 	exit
 fi
-
-
 
 for i in `seq 1 $3`
 do
@@ -35,12 +29,10 @@ done
 #./generate_matches.py --number $binSize --from $start --to $end
 
 ./generate_matches.py -n $1
-
 echo generate_matches.py complete
 
 ./bin_by_duration.py -n $1 -b $2
-
 echo bin_by_duration.py complete
 
 ./bin_by_weight.py -n $1 -b $2
-
+echo bin_by_weight.py complete
